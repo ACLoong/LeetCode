aa93d39942249a08811042b968eaa89dc115928d
zmzhouXJTU <zzm20132616@gmail.com>
2019-01-09 18:12:50 +0800

    refine the code of 226

--- src/226-Invert-Binary-Tree/226._Invert_Binary_Tree.py
+++ src/226-Invert-Binary-Tree/226._Invert_Binary_Tree.py
@@ -20,6 +20,6 @@ class Solution:
         if root is None:
             return None
 
-        if root is None:
+        if root is not None:
             root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
         return root
