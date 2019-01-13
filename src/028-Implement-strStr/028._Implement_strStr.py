b73fd799542debd008c1dd111a7f7b217fd86b84
zmzhouXJTU <zzm20132616@gmail.com>
2019-01-14 00:02:20 +0800

    refine the code of leetcode 028

--- src/028-Implement-strStr/028._Implement_strStr.py
+++ src/028-Implement-strStr/028._Implement_strStr.py
@@ -12,14 +12,17 @@ class Solution:
         """
         l1 = len(haystack)
         l2 = len(needle)
+
         if l1 < l2:
             return -1
+
         end = l2
         start = 0
+
         while end <= l1:
             if haystack[start:end] == needle:
                 return start
             start +=1
             end +=1
+
         return -1
-   
